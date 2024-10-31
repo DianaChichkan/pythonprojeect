70e2b329782a60fc76975f2415870d10a07513da
Diana Chichkan <diana4621@mail.ru>
2024-11-01 01:50:14 +0300

    init commit

--- src/decorators.py
+++ src/decorators.py
@@ -1,55 +1,57 @@
 from functools import wraps
-from typing import Callable, Any
+from typing import Callable, Any, Optional
 from time import time
 
 
-def log(filename="mylog.txt") -> Callable:
+def log(filename: Optional[str] = None) -> Callable:
     """Декоратор для логирования функции, аргументов, результатов и ошибок"""
-    def decorator(func: Callable) -> Callable:
+
+    def decorator(func: Any) -> Any:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
-            result = func(*args, **kwargs)
             try:
+                result = func(*args, **kwargs)
                 if filename is not None:
-                    with open(filename, "a") as file:
-                         file.write(f"\n{func.__name__} ok")
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} ok\n")
                 else:
                     print(f"{func.__name__} ok")
                 return result
             except Exception as error:
-                if filename is not None:
-                    with open(filename, "a") as file:
-                         file.write(f"\n{func.__name__} error: {error.__class__.__name__}. Inputs: {args}, {kwargs}")
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} error: {error.__class__.__name__}. Inputs: {args}, {kwargs}\n")
                 else:
                     print(f"{func.__name__} error: {error.__class__.__name__}. Inputs: {args}, {kwargs}")
-                raise error
+
         return wrapper
+
     return decorator
 
 
 def printing(func):
-    def wrapper(*args, **kwargs) :
-          print(f'Function {func} started')
-          result = func(*args, **kwargs)
-          print(f'Function {func} finished')
-          return result
+    def wrapper(*args, **kwargs):
+        print(f"Function {func} started")
+        result = func(*args, **kwargs)
+        print(f"Function {func} finished")
+        return result
+
     return wrapper
 
 
 def timer(func):
-   def wrapper(*args, **kwargs):
-       time_1 = time()
-       result = func(*args, **kwargs)
-       time_2 = time()
-       print(f'Time for work: {time_2 - time_1}')
-       return result
-   return wrapper
+    def wrapper(*args, **kwargs):
+        time_1 = time()
+        result = func(*args, **kwargs)
+        time_2 = time()
+        print(f"Time for work: {time_2 - time_1}")
+        return result
+
+    return wrapper
 
 
 @printing
 @timer
 def my_function():
-    for i in range (100000000):
+    for i in range(100000000):
         continue
-
-my_function()
