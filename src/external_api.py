14b69eec68ead257c1539e67a02fd0a1f60f4222
Diana Chichkan <diana4621@mail.ru>
2025-01-08 22:03:05 +0300

    init commit

--- src/external_api.py
+++ src/external_api.py
@@ -1,7 +1,4 @@
-from locale import currency
-
 import requests
-import json
 import os
 from dotenv import load_dotenv
 from src.utils import get_transactions
@@ -9,6 +6,7 @@ from src.utils import get_transactions
 load_dotenv()
 list_of_transactions = get_transactions(r"..\data\operation.json")
 print(list_of_transactions)
+
 
 def get_transaction_amount(transaction: dict) -> float:
     """Функция принимает на вход сумму транзакцию и производит конвертацию в рубли"""
@@ -24,16 +22,17 @@ def get_transaction_amount(transaction: dict) -> float:
     except Exception as e:
         print(e)
 
+
 def get_convert_from_to(from_currency: str, to_currency: str, amount: float) -> float:
     API_KEY = os.getenv("API_KEY")
-    url = f'https://api.apilayer.com/exchangerates_data/convert?to={to_currency}&from={from_currency}&amount={amount}'
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to_currency}&from={from_currency}&amount={amount}"
 
     payload = {}
     headers = {"apikey": API_KEY}
 
     response = requests.request("GET", url, headers=headers, data=payload)
     json = response.json()
-    return json['query']['amount']
+    return json["query"]["amount"]
 
 
 for transaction in list_of_transactions:
